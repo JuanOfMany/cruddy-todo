a617d6bb353ef3792846a7bd457e4976fab60296
Sam Bartlett <96403295+samkbe@users.noreply.github.com>
2022-06-22 19:29:23 -0400

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -60,13 +60,15 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  exports.readOne(id, function(error) {
+    if (error) {
+      callback(error);
+    } else {
+      fs.writeFile((path.join(exports.dataDir, `${id}.txt`)), text, function(error) {
+        callback(error);
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
