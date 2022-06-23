9ee4bb8742c6e3e3e8e94b4a2cae8bce97fae8f3
Sam Bartlett <96403295+samkbe@users.noreply.github.com>
2022-06-22 20:40:33 -0400

    Refactor readAll in progress...

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const fsPromises = require('fs').promises;
 
 var items = {};
 
@@ -20,32 +21,66 @@ exports.create = (text, callback) => {
   });
 };
 
-exports.readAll = (callback) => {
-  var resultArr = [];
-  fs.readdir(exports.dataDir, function (err, filenames) {
-    if (err) {
-      // onError(err);
-      // return;
-    }
-    if (filenames.length === 0) {
-      callback(err, []);
-    }
-    filenames.forEach(function (filename, i) {
-      fs.readFile(exports.dataDir + filename, 'utf-8', function (err, content) {
-        if (err) {
-          // onError(err);
-          // return;
-        }
-        var todoObj = {id: filename.slice(0, 5), text: filename.slice(0, 5)};
-        resultArr.push(todoObj);
 
-        if (i === (filenames.length - 1)) {
-          callback(err, resultArr);
-        }
-      });
+exports.readAll = (callback) => {
+
+  var currentFiles = fs.readdirSync( exports.dataDir );
+
+  return new Promise(function (resolve, reject) {
+    let routesArray = [];
+    const promises = currentFiles.map(file => { // get back an array of promises
+      return fs.readFile(exports.dataDir + `/${file}`)
+        .then((routes) => {
+          console.log('routes: ' + routes);
+          routesArray.push(...routes); // I like .push function
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     });
+    Promise.all(promises)
+      .then(() => { // all done!
+        console.log(routesArray);
+        resolve(routesArray);
+      });
   });
 };
+
+
+
+  // var currentFiles = fs.readdirSync( exports.dataDir );
+
+  // var promisesArray = currentFiles.map(function (file) {
+  //   fsPromises.readFile(exports.dataDir + `/${file}.txt`, 'utf-8');
+  // });
+  // console.log(promisesArray);
+
+
+  // var resultArr = [];
+  // fs.readdir(exports.dataDir, function (err, filenames) {
+  //   if (err) {
+  //     // onError(err);
+  //     // return;
+  //   }
+  //   if (filenames.length === 0) {
+  //     callback(err, []);
+  //   }
+  //   filenames.forEach(function (filename, i) {
+  //     fs.readFile(exports.dataDir + filename, 'utf-8', function (err, content) {
+  //       if (err) {
+  //         // onError(err);
+  //         // return;
+  //       }
+  //       var todoObj = {id: filename.slice(0, 5), text: filename.slice(0, 5)};
+  //       resultArr.push(todoObj);
+
+  //       if (i === (filenames.length - 1)) {
+  //         callback(err, resultArr);
+  //       }
+  //     });
+  //   });
+  // });
+// };
 
 exports.readOne = (id, callback) => {
   fs.readFile((path.join(exports.dataDir, `${id}.txt`)), 'utf-8', function (err, data) {
@@ -73,23 +108,8 @@ exports.update = (id, text, callback) => {
 
 exports.delete = (id, callback) => {
   fs.unlink((path.join(exports.dataDir, `${id}.txt`)), function(error) {
-    if (error) {
-      callback(error);
-    } else {
-      callback(error);
-    }
-
+    callback(error);
   });
-
-
-  // var item = items[id];
-  // delete items[id];
-  // if (!item) {
-  //   // report an error if item not found
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   callback();
-  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
